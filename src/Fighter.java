94d6729c2794372f8c369ac2c32cc25061b0397d
ericc346 <ericc346@nycstudents.net>
2024-12-16 13:16:31 -0500

    Update Fighter.java

--- src/Fighter.java
+++ src/Fighter.java
@@ -1,2 +1,19 @@
-public class Fighter {
+public class Fighter extends Avatar {
+
+
+    public Fighter(String name, double health){
+        this.name = name;
+        this.health = health;
+        this.type = "Fighter";
+        this.ability = "Charge";
+        this.vl = "AHHHHH!";
+        this.dm = 1.25;
+        this.rm = 0.75;
+    }
+
+    public void gainResistance(){
+        int decrease = (int) (Math.random()*11)+1;
+        this.rm = this.rm - (int) (Math.random()/4);
+        this.health = health - decrease; 
+    }
 }
