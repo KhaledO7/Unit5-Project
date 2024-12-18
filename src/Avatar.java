1b1b2840929ae26ee72b118ee690b2ff7b8e9775
BT_3W2_26 <khaledo@nycstudents.net>
2024-12-18 13:21:18 -0500

    Work completed: created a main class tested the functions of the voiceline and take damage created a new variable, abilitydmg created a new avatar
    work to complete:
    assassinaton class
    tank class
    healer class
    
    ideas:
    interactive methods?
    Visuals (complicated)

--- src/Avatar.java
+++ src/Avatar.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Avatar {
     public double health;
     public double rm = 1.0;
@@ -6,9 +7,10 @@ public class Avatar {
     public double dm = 1.0;
     public String vl;
     public String ability;
+    public double abDmg;
     public boolean isAlive;
 
-    public Avatar( String name, double health, String type, String ability, String vl, double dm, double rm, boolean alive){
+    public Avatar( String name, double health, String type, String ability, String vl, double dm, double rm, boolean alive, double abDmg){
         this.name = name;
         this.health = health;
         this.type = type;
@@ -16,6 +18,7 @@ public class Avatar {
         this.vl = vl;
         this.dm = dm;
         this.isAlive = alive;
+        this.abDmg = abDmg;
     }
 
     public Avatar(){
@@ -25,6 +28,7 @@ public class Avatar {
         this.ability = "Beg!";
         this.vl = "Don't hurt us! Please! Resort to peace!";
         this.dm = 1.2;
+        this.abDmg = 45;
 
     }
 
@@ -62,6 +66,22 @@ public class Avatar {
 
     public double getDm() {
         return dm;
+    }
+
+    public double getAbDmg() {
+        return abDmg;
+    }
+
+    public void setAbDmg(double abDmg) {
+        this.abDmg = abDmg;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
     }
 
     public void setDm(double dm) {
@@ -103,14 +123,25 @@ public class Avatar {
     public void gainHealth(){
         int increase = (int) (Math.random()*11)+1;
         this.health = health + increase;
+        System.out.println("health is: ");
+        System.out.println(health);
     }
 
     public double takeDamage(double damage){
         damage = damage * rm;
         health = health - damage;
+        System.out.println(" you took " + damage + " points of damage!");
+        System.out.println(" Current HP: ");
+        System.out.println(health);
         return health;
     }
-
+public static void main(String[]args){
+        Avatar Steve = new Avatar("Steve", 150.0,"C","Fruit Punch", "Ahahahaha! This fist is called 'fruit'! And this one is 'punch'! It's fruit punch, get it?!",1.1,1.1,true,100);
+        Steve.voiceline();
+        Avatar Chris = new Avatar();
+        Chris.voiceline();
+        Chris.takeDamage(40.0);
+}
 
 }
 
